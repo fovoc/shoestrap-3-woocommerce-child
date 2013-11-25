34ae8d7ad57b1ffac8ac8e2d28e1b1d6fd9623df
Aristeides Stathopoulos <aristath@gmail.com>
2013-11-25 21:27:17 +0200

    undo latest commits

--- templates/content-single-product.php
+++ templates/content-single-product.php
@@ -7,10 +7,6 @@ global $post, $product;
 do_action( 'woocommerce_before_single_product' ); ?>
 
 <div itemscope itemtype="http://schema.org/Product" id="product-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php if ($product->is_on_sale()) :
-		echo apply_filters( 'woocommerce_sale_flash', '<span class="onsale">' . __( 'Sale!', 'woocommerce' ) . '</span>', $post, $product);
-	endif; ?>
-
 	<?php do_action( 'woocommerce_before_single_product_summary' ); ?>
 	<div class="summary entry-summary">
 		<?php do_action( 'woocommerce_single_product_summary' ); ?>
