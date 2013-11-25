e45b9592eff66388f9cf6ec927fc255570e80563
Aristeides Stathopoulos <aristath@gmail.com>
2013-11-25 21:23:19 +0200

    adding sale-flash inside the single template file

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 
 // Remove default WooCommerce titles
-add_filter('woocommerce_show_page_title', '__return_false');
-remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_title', 5);
+add_filter( 'woocommerce_show_page_title', '__return_false' );
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
+remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_sale_flash', 10 );
