951a2b5970f69e9b17bbc868b02c50ef4961739f
Aristeides Stathopoulos <aristath@gmail.com>
2013-11-25 20:36:20 +0200

    Remove default WooCommerce titles

---
+++ functions.php
@@ -0,0 +1,5 @@
+<?php
+
+// Remove default WooCommerce titles
+add_filter('woocommerce_show_page_title', '__return_false');
+remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_title', 5);
