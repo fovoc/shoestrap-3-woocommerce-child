0b77a939728a073f57bcf94a80f58973697a4e8e
Aristeides Stathopoulos <aristath@gmail.com>
2013-11-26 13:02:09 +0200

    add to cart buttons (first pass)

--- functions.php
+++ functions.php
@@ -1,12 +1,23 @@
 <?php
 
+define('WOOCOMMERCE_USE_CSS', false);
+add_filter( 'woocommerce_enqueue_styles', '__return_false' );
+add_theme_support( 'woocommerce' );
+
 // Remove default WooCommerce titles
 add_filter( 'woocommerce_show_page_title', '__return_false' );
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
 
-
-remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
-
-add_action( 'woocommerce_after_single_product_summary', function() {
-	get_template_part( 'templates/single-product/tabs/tabs' ); 
-}, 10 );
+function shoestrap_woocommerce_styles() { ?>
+	<style>
+	div.quantity.buttons_added {
+		width: 50px;
+	}
+	div.quantity.buttons_added .minus,
+	div.quantity.buttons_added .plus {
+		display: inline-block;
+	}
+	</style>
+	<?php
+}
+add_action( 'wp_footer', 'shoestrap_woocommerce_styles' );
