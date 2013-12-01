8a06ecafd0af0cca5f21941c8858d1f4689d50db
Aristeides Stathopoulos <aristath@gmail.com>
2013-12-01 11:51:11 +0200

    added image calculation

--- functions.php
+++ functions.php
@@ -48,3 +48,27 @@ function shoestrap_woocommerce_styles() { ?>
 	<?php
 }
 add_action( 'wp_footer', 'shoestrap_woocommerce_styles' );
+
+
+remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10 );
+add_action( 'woocommerce_before_shop_loop_item_title', 'shoestrap_woocommerce_template_loop_product_thumbnail', 10 );
+
+function shoestrap_woocommerce_template_loop_product_thumbnail() {
+	echo shoestrap_woocommerce_get_product_thumbnail();
+}
+
+function shoestrap_woocommerce_get_product_thumbnail( $size = 'shop_catalog', $placeholder_width = 0, $placeholder_height = 0  ) {
+	global $post;
+
+	$data['width']  = ( shoestrap_content_width_px() / 2 - shoestrap_getVariable( 'layout_gutter' ) );
+	$data['url']    = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );
+	$data['height'] = $data['width'];
+
+	$image = shoestrap_image_resize( $data );
+	$image = '<a href="' . get_permalink() . '"><img class="featured-image" src="' . $image['url'] . '" /></a>';
+
+	if ( has_post_thumbnail() )
+		return $image;
+	elseif ( wc_placeholder_img_src() )
+		return wc_placeholder_img( $size );
+}
