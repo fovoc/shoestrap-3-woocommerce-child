5121d7e37b8cc75714092ead12b19806b8881649
Aristeides Stathopoulos <aristath@gmail.com>
2013-12-18 15:36:37 +0200

    syntax improvements

--- single-product/add-to-cart/simple.php
+++ single-product/add-to-cart/simple.php
@@ -1,57 +1,45 @@
 <?php
-/**
- * Simple product add to cart
- *
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
- * @version     2.0.1
- */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 global $woocommerce, $product;
 
-if ( ! $product->is_purchasable() ) return;
-?>
+if ( !$product->is_purchasable() ) :
+	return;
+endif;
 
-<?php
-	// Availability
-	$availability = $product->get_availability();
+$availability = $product->get_availability();
 
-	if ($availability['availability']) :
-		echo apply_filters( 'woocommerce_stock_html', '<p class="stock ' . esc_attr( $availability['class'] ) . '">' . esc_html( $availability['availability'] ) . '</p>', $availability['availability'] );
-    endif;
-?>
+if ($availability['availability']) :
+	echo apply_filters( 'woocommerce_stock_html', '<p class="stock ' . esc_attr( $availability['class'] ) . '">' . esc_html( $availability['availability'] ) . '</p>', $availability['availability'] );
+endif;
 
-<?php if ( $product->is_in_stock() ) : ?>
-
+if ( $product->is_in_stock() ) : ?>
 	<?php do_action('woocommerce_before_add_to_cart_form'); ?>
 	<hr>
-	
-	<div class="row hsaddtocart">
-	<form action="<?php echo esc_url( $product->add_to_cart_url() ); ?>" class="cart form" method="post" enctype='multipart/form-data'>
 
-	 	<?php do_action('woocommerce_before_add_to_cart_button'); ?>
+	<div class="row">
+		<form action="<?php echo esc_url( $product->add_to_cart_url() ); ?>" class="cart form" method="post" enctype='multipart/form-data'>
+			<?php do_action('woocommerce_before_add_to_cart_button'); ?>
 
-	 	<div class="col-xs-6 col-sm-4">
-	 	<?php
-	 		if ( ! $product->is_sold_individually() )
-	 			woocommerce_quantity_input( array(
-	 				'min_value' => apply_filters( 'woocommerce_quantity_input_min', 1, $product ),
-	 				'max_value' => apply_filters( 'woocommerce_quantity_input_max', $product->backorders_allowed() ? '' : $product->get_stock_quantity(), $product )
-	 			) );
-	 	?>	
-	 	</div>
+			<div class="col-xs-6 col-sm-4">
+				<?php
+					if ( ! $product->is_sold_individually() ) :
+						woocommerce_quantity_input( array(
+							'min_value' => apply_filters( 'woocommerce_quantity_input_min', 1, $product ),
+							'max_value' => apply_filters( 'woocommerce_quantity_input_max', $product->backorders_allowed() ? '' : $product->get_stock_quantity(), $product )
+						) );
+					endif;
+				?>
+			</div>
 
-	 	<div class="col-xs-6 col-sm-8 buttonblock">
-	 		<button type="submit" class="btn btn-md theme single_add_to_cart_button button alt"><?php echo apply_filters('single_add_to_cart_text', __('Add to cart', 'woocommerce'), $product->product_type); ?></button>
-	 	</div>
-
-	 	<?php do_action('woocommerce_after_add_to_cart_button'); ?>
-
-	</form>
+			<div class="col-xs-6 col-sm-8 buttonblock">
+				<button type="submit" class="btn btn-md theme single_add_to_cart_button button alt"><?php echo apply_filters('single_add_to_cart_text', __('Add to cart', 'woocommerce'), $product->product_type); ?></button>
+			</div>
+			<?php do_action('woocommerce_after_add_to_cart_button'); ?>
+		</form>
 	</div>
 	<hr>
-	<?php do_action('woocommerce_after_add_to_cart_form'); ?>
-
-<?php endif; ?>
+	<?php
+	do_action('woocommerce_after_add_to_cart_form');
+endif;
--- single-product/add-to-cart/variable.php
+++ single-product/add-to-cart/variable.php
@@ -1,104 +1,89 @@
 <?php
-/**
- * Variable product add to cart
- *
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
- * @version     2.0.3
- */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-global $woocommerce, $product, $post;
-?>
+global $woocommerce, $product, $post; ?>
 
 <?php do_action('woocommerce_before_add_to_cart_form'); ?>
 <hr>
 <form action="<?php echo esc_url( $product->add_to_cart_url() ); ?>" class="variations_form cart form" method="post" enctype='multipart/form-data' data-product_id="<?php echo $post->ID; ?>" data-product_variations="<?php echo esc_attr( json_encode( $available_variations ) ) ?>">
 	<div class="variations" cellspacing="0">
-		
-			<?php $loop = 0; foreach ( $attributes as $name => $options ) : $loop++; ?>
-				<div class="row option">
-					<div class="col-sm-8"><label for="<?php echo sanitize_title($name); ?>"><?php echo $woocommerce->attribute_label( $name ); ?></label></div>
-					<div class="col-sm-4 value"><select id="<?php echo esc_attr( sanitize_title($name) ); ?>" name="attribute_<?php echo sanitize_title($name); ?>" class="form-control">
+		<?php $loop = 0; ?>
+		<?php foreach ( $attributes as $name => $options ) : $loop++; ?>
+			<div class="row option">
+				<div class="col-sm-8"><label for="<?php echo sanitize_title($name); ?>"><?php echo $woocommerce->attribute_label( $name ); ?></label></div>
+				<div class="col-sm-4 value">
+					<select id="<?php echo esc_attr( sanitize_title($name) ); ?>" name="attribute_<?php echo sanitize_title($name); ?>" class="form-control">
 						<option value=""><?php echo __( 'Choose an option', 'woocommerce' ) ?>&hellip;</option>
 						<?php
-							if ( is_array( $options ) ) {
+							if ( is_array( $options ) ) :
 
-								if ( empty( $_POST ) )
+								if ( empty( $_POST ) ) :
 									$selected_value = ( isset( $selected_attributes[ sanitize_title( $name ) ] ) ) ? $selected_attributes[ sanitize_title( $name ) ] : '';
-								else
+								else :
 									$selected_value = isset( $_POST[ 'attribute_' . sanitize_title( $name ) ] ) ? $_POST[ 'attribute_' . sanitize_title( $name ) ] : '';
+								endif;
 
-								// Get terms if this is a taxonomy - ordered
-								if ( taxonomy_exists( $name ) ) {
-
+								if ( taxonomy_exists( $name ) ) :
 									$orderby = $woocommerce->attribute_orderby( $name );
 
-									switch ( $orderby ) {
+									switch ( $orderby ) :
 										case 'name' :
 											$args = array( 'orderby' => 'name', 'hide_empty' => false, 'menu_order' => false );
 										break;
+
 										case 'id' :
 											$args = array( 'orderby' => 'id', 'order' => 'ASC', 'menu_order' => false );
 										break;
+
 										case 'menu_order' :
 											$args = array( 'menu_order' => 'ASC' );
 										break;
-									}
+									endswitch;
 
 									$terms = get_terms( $name, $args );
 
-									foreach ( $terms as $term ) {
-										if ( ! in_array( $term->slug, $options ) )
+									foreach ( $terms as $term ) :
+										if ( !in_array( $term->slug, $options ) ) :
 											continue;
-
+										endif;
 										echo '<option value="' . esc_attr( $term->slug ) . '" ' . selected( $selected_value, $term->slug, false ) . '>' . apply_filters( 'woocommerce_variation_option_name', $term->name ) . '</option>';
-									}
-								} else {
-
-									foreach ( $options as $option ) {
+									endforeach;
+								else :
+									foreach ( $options as $option ) :
 										echo '<option value="' . esc_attr( sanitize_title( $option ) ) . '" ' . selected( sanitize_title( $selected_value ), sanitize_title( $option ), false ) . '>' . esc_html( apply_filters( 'woocommerce_variation_option_name', $option ) ) . '</option>';
-									}
-
-								}
-							}
+									endforeach;
+								endif;
+							endif;
 						?>
 					</select>
-					</div>
 				</div>
-	        <?php endforeach;?>
-	        <hr>
+			</div>
+		<?php endforeach;?>
+		<hr>
 	</div>
 
-	<?php do_action('woocommerce_before_add_to_cart_button'); ?>
+	<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
 
 	<div class="single_variation_wrap" style="display:none;">
-		
 		<div class="single_variation"></div>
-		
-
 		<div class="clearfix">
 			<div class="pull-left">
 				<div class="variations_button">
-
 					<input type="hidden" name="variation_id" value="" />
 					<?php woocommerce_quantity_input(); ?>
-					
 				</div>
 			</div>
 			<div class="pull-right">
-				<?php if ( sizeof($attributes) == $loop )
-				echo '<small><a class="reset_variations" href="#reset">&nbsp;&nbsp;'.__('Clear Selection', 'woocommerce').'</a></small>'; ?> 
+				<?php if ( sizeof( $attributes ) == $loop ) : ?>
+					<small><a class="reset_variations" href="#reset">&nbsp;&nbsp;<?php _e( 'Clear Selection', 'woocommerce' ); ?></a></small>
+				<?php endif; ?>
 				<button type="submit" class="btn theme single_add_to_cart_button alt"><?php echo apply_filters('single_add_to_cart_text', __( 'Add to cart', 'woocommerce' ), $product->product_type); ?></button>
-			</div>	
+			</div>
 		</div>
-		<hr>
 	</div>
+	<hr>
 	<div><input type="hidden" name="product_id" value="<?php echo esc_attr( $post->ID ); ?>" /></div>
-
 	<?php do_action('woocommerce_after_add_to_cart_button'); ?>
-
 </form>
-
-<?php do_action('woocommerce_after_add_to_cart_form'); ?>
+<?php do_action('woocommerce_after_add_to_cart_form');
