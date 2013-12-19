51a07808696847c55a7087dd38fb102da207fdb0
Aristeides Stathopoulos <aristath@gmail.com>
2013-12-19 17:51:30 +0200

    styling prices

--- single-product/price.php
+++ single-product/price.php
@@ -5,7 +5,7 @@ if ( !defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 global $post, $product; ?>
 
 <div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
-	<h4 itemprop="price" class="price"><i class="fa fa-tag"></i> <?php echo $product->get_price_html(); ?></h4>
+	<h4 itemprop="price" class="price"><i class="el-icon-tag"></i> <?php echo $product->get_price_html(); ?></h4>
 	<meta itemprop="priceCurrency" content="<?php echo get_woocommerce_currency(); ?>" />
 	<link itemprop="availability" href="http://schema.org/<?php echo $product->is_in_stock() ? 'InStock' : 'OutOfStock'; ?>" />
 </div>
