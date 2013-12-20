0c32098c7c8e4ec483e86a473efa063d6bf719d5
Aristeides Stathopoulos <aristath@gmail.com>
2013-12-20 22:40:18 +0200

    updated template files for single-product/add-to-cart to 2.1.0

--- woocommerce/single-product/add-to-cart/simple.php
+++ woocommerce/single-product/add-to-cart/simple.php
@@ -1,45 +1,57 @@
 <?php
+/**
+ * Simple product add to cart
+ *
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     2.1.0
+ */
 
-if ( !defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 global $woocommerce, $product;
 
-if ( !$product->is_purchasable() ) :
-	return;
-endif;
+if ( ! $product->is_purchasable() ) return;
+?>
 
-$availability = $product->get_availability();
+<?php
+	// Availability
+	$availability = $product->get_availability();
 
-if ($availability['availability']) :
-	echo apply_filters( 'woocommerce_stock_html', '<p class="stock ' . esc_attr( $availability['class'] ) . '">' . esc_html( $availability['availability'] ) . '</p>', $availability['availability'] );
-endif;
+	if ($availability['availability'])
+		echo apply_filters( 'woocommerce_stock_html', '<p class="stock ' . esc_attr( $availability['class'] ) . '">' . esc_html( $availability['availability'] ) . '</p>', $availability['availability'] );
+?>
 
-if ( $product->is_in_stock() ) : ?>
+<?php if ( $product->is_in_stock() ) : ?>
+<hr>
+<div class="row">
+
 	<?php do_action('woocommerce_before_add_to_cart_form'); ?>
-	<hr>
 
-	<div class="row">
-		<form action="<?php echo esc_url( $product->add_to_cart_url() ); ?>" class="cart form" method="post" enctype='multipart/form-data'>
-			<?php do_action('woocommerce_before_add_to_cart_button'); ?>
+	<form class="cart" method="post" enctype='multipart/form-data'>
+	 	<?php do_action('woocommerce_before_add_to_cart_button'); ?>
+	 	<div class="col-sm-4">
 
-			<div class="col-sm-4">
-				<?php
-					if ( ! $product->is_sold_individually() ) :
-						woocommerce_quantity_input( array(
-							'min_value' => apply_filters( 'woocommerce_quantity_input_min', 1, $product ),
-							'max_value' => apply_filters( 'woocommerce_quantity_input_max', $product->backorders_allowed() ? '' : $product->get_stock_quantity(), $product )
-						) );
-					endif;
-				?>
-			</div>
+	 	<?php
+	 		if ( ! $product->is_sold_individually() )
+	 			woocommerce_quantity_input( array(
+	 				'min_value' => apply_filters( 'woocommerce_quantity_input_min', 1, $product ),
+	 				'max_value' => apply_filters( 'woocommerce_quantity_input_max', $product->backorders_allowed() ? '' : $product->get_stock_quantity(), $product )
+	 			) );
+	 	?>
+	 	</div>
 
-			<div class="col-sm-8">
-				<button type="submit" class="btn btn-block btn-primary btn-lg"><?php echo apply_filters('single_add_to_cart_text', __('Add to cart', 'woocommerce'), $product->product_type); ?></button>
-			</div>
-			<?php do_action('woocommerce_after_add_to_cart_button'); ?>
-		</form>
-	</div>
-	<hr>
-	<?php
-	do_action('woocommerce_after_add_to_cart_form');
-endif;
+	 	<div class="col-sm-8">
+
+	 	<input type="hidden" name="add-to-cart" value="<?php echo esc_attr( $product->id ); ?>" />
+
+	 	<button type="submit" class="single_add_to_cart_button btn btn-lg btn-primary btn-block"><?php echo $product->single_add_to_cart_text(); ?></button>
+	 	</div>
+
+		<?php do_action('woocommerce_after_add_to_cart_button'); ?>
+	</form>
+
+	<?php do_action('woocommerce_after_add_to_cart_form'); ?>
+</div>
+<hr>
+<?php endif; ?>
