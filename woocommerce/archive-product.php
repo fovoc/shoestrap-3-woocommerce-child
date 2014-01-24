53d6ebb42322bdc739e995e8be095328b31ce3f5
Aristeides Stathopoulos <aristath@gmail.com>
2014-01-24 17:14:35 +0200

    bugfix

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -10,16 +10,14 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-?>
 
-	<?php
 		/**
 		 * woocommerce_before_main_content hook
 		 *
 		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
 		 * @hooked woocommerce_breadcrumb - 20
 		 */
-		do_action('woocommerce_before_main_content');
+		do_action( 'woocommerce_before_main_content' );
 	?>
 
 		<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
@@ -75,4 +73,4 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 		 *
 		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
 		 */
-		do_action('woocommerce_after_main_content');
+		do_action( 'woocommerce_after_main_content' );
