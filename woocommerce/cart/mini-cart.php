926d49d7d453f440aba15c1c00039ce1d84118f5
Aristeides Stathopoulos <aristath@gmail.com>
2014-02-17 22:09:51 +0200

    cart template fixes

--- woocommerce/cart/mini-cart.php
+++ woocommerce/cart/mini-cart.php
@@ -6,7 +6,7 @@
  *
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     1.6.4
+ * @version     2.1.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
@@ -18,58 +18,60 @@ global $woocommerce;
 
 <ul class="cart_list product_list_widget <?php echo $args['list_class']; ?>">
 
-	<?php if ( sizeof( $woocommerce->cart->get_cart() ) > 0 ) : ?>
+	<?php if ( sizeof( WC()->cart->get_cart() ) > 0 ) : ?>
 
-		<?php foreach ( $woocommerce->cart->get_cart() as $cart_item_key => $cart_item ) :
+		<?php
+			foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
+				$_product     = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
+				$product_id   = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
 
-			$_product = $cart_item['data'];
+				if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_widget_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
 
-			// Only display if allowed
-			if ( ! apply_filters('woocommerce_widget_cart_item_visible', true, $cart_item, $cart_item_key ) || ! $_product->exists() || $cart_item['quantity'] == 0 )
-				continue;
+					$product_name  = apply_filters( 'woocommerce_cart_item_name', $_product->get_title(), $cart_item, $cart_item_key );
+					$thumbnail     = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
+					$product_price = apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key );
 
-			// Get price
-			$product_price = get_option( 'woocommerce_tax_display_cart' ) == 'excl' ? $_product->get_price_excluding_tax() : $_product->get_price_including_tax();
+					?>
+					<li class="row">
+						<div class="col-xs-4">
+							<a href="<?php echo get_permalink( $product_id ); ?>">
+								<?php echo $thumbnail; ?>
+							</a>
+						</div>
+						<div class="col-xs-8">
+							<a href="<?php echo get_permalink( $product_id ); ?>">
+								<?php echo $product_name; ?>
+							</a>
+						</div>
 
-			$product_price = apply_filters( 'woocommerce_cart_item_price_html', woocommerce_price( $product_price ), $cart_item, $cart_item_key );
-			?>
+						<?php echo WC()->cart->get_item_data( $cart_item ); ?>
 
-			<li class="row">
-				<div class="col-xs-4">
-					<a href="<?php echo get_permalink( $cart_item['product_id'] ); ?>">
-						<?php echo $_product->get_image(); ?>
-					</a>
-				</div>
-				<div class="col-xs-8">
-					<a href="<?php echo get_permalink( $cart_item['product_id'] ); ?>">
-						<?php echo apply_filters('woocommerce_widget_cart_product_title', $_product->get_title(), $_product ); ?>
-					</a>
-					<p><small><?php echo $woocommerce->cart->get_item_data( $cart_item ); ?> <?php echo apply_filters( 'woocommerce_widget_cart_item_quantity', '<span>' . sprintf( '%s &times; %s', $cart_item['quantity'], $product_price ) . '</span>', $cart_item, $cart_item_key ); ?></small></p>
-				</div>
-			</li>
-
-		<?php endforeach; ?>
+						<?php echo apply_filters( 'woocommerce_widget_cart_item_quantity', '<span class="quantity">' . sprintf( '%s &times; %s', $cart_item['quantity'], $product_price ) . '</span>', $cart_item, $cart_item_key ); ?>
+					</li>
+					<?php
+				}
+			}
+		?>
 
 	<?php else : ?>
 
-		<li class="empty"><?php _e('No products in the cart.', 'woocommerce'); ?></li>
+		<li class="empty"><?php _e( 'No products in the cart.', 'woocommerce' ); ?></li>
 
 	<?php endif; ?>
 
 </ul><!-- end product list -->
 
-<?php if ( sizeof( $woocommerce->cart->get_cart() ) > 0 ) : ?>
+<?php if ( sizeof( WC()->cart->get_cart() ) > 0 ) : ?>
 
-	<hr />
-	<p class="total text-center"><strong><?php _e('Subtotal', 'woocommerce'); ?>:</strong> <?php echo $woocommerce->cart->get_cart_subtotal(); ?></p>
+	<p class="total"><strong><?php _e( 'Subtotal', 'woocommerce' ); ?>:</strong> <?php echo WC()->cart->get_cart_subtotal(); ?></p>
 
 	<?php do_action( 'woocommerce_widget_shopping_cart_before_buttons' ); ?>
 
-	<p class="text-center">
-		<a href="<?php echo $woocommerce->cart->get_cart_url(); ?>" class="btn btn-default"><?php _e('View Cart', 'woocommerce'); ?></a>
-		<a href="<?php echo $woocommerce->cart->get_checkout_url(); ?>" class="btn btn-default checkout"><?php _e('Checkout <i class="el-icon-shopping-cart"></i>', 'woocommerce'); ?></a>
+	<p class="buttons text-center">
+		<a href="<?php echo WC()->cart->get_cart_url(); ?>" class="btn btn-default wc-forward"><?php _e( 'View Cart', 'woocommerce' ); ?></a>
+		<a href="<?php echo WC()->cart->get_checkout_url(); ?>" class="btn btn-default checkout wc-forward"><?php _e( 'Checkout', 'woocommerce' ); ?></a>
 	</p>
-	<hr />
+
 <?php endif; ?>
 
-<?php do_action( 'woocommerce_after_mini_cart' );
+<?php do_action( 'woocommerce_after_mini_cart' ); ?>
